e60dac0ad789ab57f6ad5da80df0730788535d0b
Qiushi Han <qh23@rice.edu>
2026-04-26 18:16:44 -0500

    Fix DBSCAN cluster expansion bug with visited tracking

--- src/rice_ml/unsupervised_learning/dbscan.py
+++ src/rice_ml/unsupervised_learning/dbscan.py
@@ -14,16 +14,18 @@ class DBSCAN:
         X = np.asarray(X, dtype=float)
         n = len(X)
         self.labels_ = np.full(n, -1)
+        visited = np.zeros(n, dtype=bool)
         cluster_id = 0
 
         for i in range(n):
-            if self.labels_[i] != -1:
+            if visited[i]:
                 continue
+            visited[i] = True
             neighbors = self._get_neighbors(X, i)
             if len(neighbors) < self.min_samples:
-                self.labels_[i] = -1  # noise
+                self.labels_[i] = -1
                 continue
-            self._expand_cluster(X, i, neighbors, cluster_id)
+            self._expand_cluster(X, i, neighbors, cluster_id, visited)
             cluster_id += 1
 
         return self
@@ -32,18 +34,20 @@ class DBSCAN:
         distances = np.linalg.norm(X - X[i], axis=1)
         return np.where(distances <= self.eps)[0]
 
-    def _expand_cluster(self, X, i, neighbors, cluster_id):
+    def _expand_cluster(self, X, i, neighbors, cluster_id, visited):
         self.labels_[i] = cluster_id
+        neighbors = list(neighbors)
         idx = 0
         while idx < len(neighbors):
             j = neighbors[idx]
-            if self.labels_[j] == -1:
-                self.labels_[j] = cluster_id
-            if self.labels_[j] == -1:
-                self.labels_[j] = cluster_id
+            if not visited[j]:
+                visited[j] = True
                 new_neighbors = self._get_neighbors(X, j)
                 if len(new_neighbors) >= self.min_samples:
-                    neighbors = np.union1d(neighbors, new_neighbors)
+                    neighbors += [n for n in new_neighbors
+                                  if n not in neighbors]
+            if self.labels_[j] == -1:
+                self.labels_[j] = cluster_id
             idx += 1
 
     def fit_predict(self, X):
